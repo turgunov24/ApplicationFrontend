effe19520a5276e2afa7e821b529c15efef66220
turgunov24 <learningplatforms.turgunov@gmail.com>
2025-08-27 22:22:42 +0500

    some

--- src/pages/core/references/assign-permissions-to-roles/index/index.tsx
+++ src/pages/core/references/assign-permissions-to-roles/index/index.tsx
@@ -1,21 +1,22 @@
-import { useQueryClient } from '@tanstack/react-query';
+import { useQueryClient } from '@tanstack/react-query'
 
-import Table from '@mui/material/Table';
-import TableRow from '@mui/material/TableRow';
-import Checkbox from '@mui/material/Checkbox';
-import TableHead from '@mui/material/TableHead';
-import TableBody from '@mui/material/TableBody';
-import { alpha, useTheme } from '@mui/material/styles';
-import Typography from '@mui/material/Typography';
-import TableContainer from '@mui/material/TableContainer';
-import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import Card from '@mui/material/Card'
+import Table from '@mui/material/Table'
+import TableRow from '@mui/material/TableRow'
+import Checkbox from '@mui/material/Checkbox'
+import TableHead from '@mui/material/TableHead'
+import TableBody from '@mui/material/TableBody'
+import { useTheme } from '@mui/material/styles'
+import Typography from '@mui/material/Typography'
+import TableContainer from '@mui/material/TableContainer'
+import TableCell, { tableCellClasses } from '@mui/material/TableCell'
 
-import { paths } from 'src/routes/paths';
+import { paths } from 'src/routes/paths'
 
-import { CONFIG } from 'src/global-config';
-import { DashboardContent } from 'src/layouts/dashboard';
+import { CONFIG } from 'src/global-config'
+import { DashboardContent } from 'src/layouts/dashboard'
 
-import { CustomBreadcrumbs } from 'src/components/custom-breadcrumbs';
+import { CustomBreadcrumbs } from 'src/components/custom-breadcrumbs'
 
 // ----------------------------------------------------------------------
 
@@ -37,65 +38,67 @@ export default function Page() {
           ]}
           sx={{ mb: { xs: 3, md: 5 } }}
         />
-        <TableContainer>
-          <Table
-            sx={{
-              [`.${tableCellClasses.root}`]: {
-                '&:nth-of-type(1)': {
-                  minWidth: theme.spacing(40),
-                  backgroundColor: 'background.default',
-                  border: 'none',
-                  boxShadow: theme.vars.customShadows.card,
+        <Card>
+          <TableContainer>
+            <Table
+              sx={{
+                [`.${tableCellClasses.root}`]: {
+                  '&:nth-of-type(1)': {
+                    minWidth: theme.spacing(40),
+                    backgroundColor: 'background.default',
+                    border: 'none',
+                    boxShadow: theme.vars.customShadows.card,
+                  },
                 },
-              },
-              [`.${tableCellClasses.head}`]: {
-                '&:nth-of-type(1)': {
-                  position: 'sticky',
-                  left: 0,
-                  top: 0,
-                  bottom: 0,
-                  zIndex: 1,
+                [`.${tableCellClasses.head}`]: {
+                  '&:nth-of-type(1)': {
+                    position: 'sticky',
+                    left: 0,
+                    top: 0,
+                    bottom: 0,
+                    zIndex: 1,
+                  },
                 },
-              },
-              [`.${tableCellClasses.body}`]: {
-                '&:nth-of-type(1)': {
-                  position: 'sticky',
-                  left: 0,
-                  zIndex: 3,
+                [`.${tableCellClasses.body}`]: {
+                  '&:nth-of-type(1)': {
+                    position: 'sticky',
+                    left: 0,
+                    zIndex: 3,
+                  },
                 },
-              },
-            }}
-          >
-            <TableHead>
-              <TableRow>
-                <TableCell>
-                  <Typography variant="h6" color="textPrimary">
-                    Permissions
-                  </Typography>
-                </TableCell>
-                {Array.from({ length: 25 }).map((_, index) => (
-                  <TableCell key={index}>Assign</TableCell>
-                ))}
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              <TableRow>
-                <TableCell>Automatic Entity Labels</TableCell>
-                <TableCell colSpan={24} />
-              </TableRow>
-              {Array.from({ length: 10 }).map((_, index) => (
-                <TableRow key={index}>
-                  <TableCell>Lorem ipsum dolor sit amet consectetur adipisici</TableCell>
-                  {Array.from({ length: 25 }).map((a, i) => (
-                    <TableCell key={i}>
-                      <Checkbox />
-                    </TableCell>
+              }}
+            >
+              <TableHead>
+                <TableRow>
+                  <TableCell>
+                    <Typography variant="h6" color="textPrimary">
+                      Permissions
+                    </Typography>
+                  </TableCell>
+                  {Array.from({ length: 25 }).map((_, index) => (
+                    <TableCell key={index}>Assign</TableCell>
                   ))}
                 </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
+              </TableHead>
+              <TableBody>
+                <TableRow>
+                  <TableCell>Automatic Entity Labels</TableCell>
+                  <TableCell colSpan={24} />
+                </TableRow>
+                {Array.from({ length: 10 }).map((_, index) => (
+                  <TableRow key={index}>
+                    <TableCell>Lorem ipsum dolor sit amet consectetur adipisici</TableCell>
+                    {Array.from({ length: 25 }).map((a, i) => (
+                      <TableCell key={i}>
+                        <Checkbox />
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Card>
       </DashboardContent>
     </>
   );
