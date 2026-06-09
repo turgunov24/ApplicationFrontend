84872b9d00c14cbdd06925c17cda6f850e8d4dbb
Murodjon Turgunov <learningplatforms.turgunov@gmail.com>
2026-06-09 21:53:36 +0500

    test pr

--- src/app.tsx
+++ src/app.tsx
@@ -20,22 +20,6 @@ import { CheckoutProvider } from 'src/sections/checkout/context';
 
 import { AuthProvider } from 'src/auth/context/jwt';
 
-// import { AuthProvider as Auth0AuthProvider } from 'src/auth/context/auth0'
-// import { AuthProvider as AmplifyAuthProvider } from 'src/auth/context/amplify'
-// import { AuthProvider as SupabaseAuthProvider } from 'src/auth/context/supabase'
-// import { AuthProvider as FirebaseAuthProvider } from 'src/auth/context/firebase'
-
-// ----------------------------------------------------------------------
-
-// const AuthProvider =
-//   (CONFIG.auth.method === 'amplify' && AmplifyAuthProvider) ||
-//   (CONFIG.auth.method === 'firebase' && FirebaseAuthProvider) ||
-//   (CONFIG.auth.method === 'supabase' && SupabaseAuthProvider) ||
-//   (CONFIG.auth.method === 'auth0' && Auth0AuthProvider) ||
-//   JwtAuthProvider;
-
-// ----------------------------------------------------------------------
-
 type AppProps = {
   children: React.ReactNode;
 };
